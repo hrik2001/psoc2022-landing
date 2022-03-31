996dd6a83ea1caa5e4c2b7ad1b5f9aabe7a223be
Ritvik Gupta <ritvikgupta75@gmail.com>
2022-03-31 14:44:29 +0530

    footer changes

--- psoc2022/components/footer.jsx
+++ psoc2022/components/footer.jsx
@@ -1,28 +1,66 @@
-import styles from '../styles/footer.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
+import styles from "../styles/footer.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const socials = [
+    {
+        src: "/img/twitter.png",
+        title: "Twitter",
+        href: "twitter.com/pclubuiet",
+    },
+    {
+        src: "/img/instagram.png",
+        title: "Instagram",
+        href: "instagram.com/pclubuiet",
+    },
+    {
+        src: "/img/email.png",
+        title: "Email",
+        href: "pclubuiet@gmail.com",
+    },
+    {
+        src: "/img/linkedin.png",
+        title: "Linkedin",
+        href: "linkedin.com/pclubuiet",
+    },
+];
 
 const Footer = () => {
-    return(
+    return (
         <div className={styles.container}>
-            <h4 className={styles.title}>Stay up to date</h4>
+            <h4 className={styles.title}>Connect With us!</h4>
             <ul className={styles.list}>
-                <li className={styles.listItem}>
-                <Link passHref href='twitter.com/pclubuiet'><Image src='/img/twitter.png' width='40px' height='40px' layout='fixed' alt=''/></Link>
-                </li>
-                <li className={styles.listItem}>
-                <Link passHref href='instagram.com/pclubuiet'><Image src='/img/instagram.png' width='40px' height='40px' layout='fixed' alt=''/></Link>
-                </li>
-                <li className={styles.listItem}>
-                <Link passHref href='pclubuiet@gmail.com'><Image src='/img/email.png' width='40px' height='40px' layout='fixed' alt=''/></Link>
-                </li>
-                <li className={styles.listItem}>
-                <Link passHref href='linkedin.com/pclubuiet'><Image src='/img/linkedin.png' width='40px' height='40px' layout='fixed' alt=''/></Link>
-                </li>
+                {socials.map((item) => (
+                    <li className={styles.listItem}>
+                        <Link passHref href={item.href}>
+                            <div className={styles.socials_item}>
+                                <Image
+                                    src={item.src}
+                                    width="30px"
+                                    height="30px"
+                                    layout="fixed"
+                                    alt=""
+                                    // className={styles.socials_image}
+                                />
+                                <h3>{item.title}</h3>
+                            </div>
+                        </Link>
+                    </li>
+                ))}
             </ul>
-            <h5 className={styles.pclub}>Made by <Image src='/img/pclub-logo.png' height='25px' width='25px' layout='fixed' alt='' /> with love ♥️.</h5>
+            <h5 className={styles.pclub}>
+                Made with ♥️ by{" "}
+                <Image
+                    src="/img/pclub-logo.png"
+                    height="50px"
+                    width="50px"
+                    layout="fixed"
+                    alt=""
+                />{" "}
+                .
+            </h5>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
