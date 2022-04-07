82f27fbed4ee62e6bb84ba1a4e94803bb4404346
Ritvik Gupta <ritvikgupta75@gmail.com>
2022-04-07 20:49:39 +0530

    Update how-it-works.jsx

--- components/how-it-works.jsx
+++ components/how-it-works.jsx
@@ -20,7 +20,7 @@ const how_it_works = [
     {
         id: 3,
         name: "Contribute",
-        title: "Contribute",
+        title: "contribute",
         desc: "Contribute your work with the Open-Source community. mentees propose a project.",
         photo: "earth.png",
     },
