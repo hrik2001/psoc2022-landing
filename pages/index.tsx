91c1a891b4cd958cd731d134e513fc2e1808400a
uav <uav0108@gmail.com>
2022-04-26 20:43:11 +0530

    created navbar and added pages & routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      
+      <Hero />
     </div>
   )
 }
