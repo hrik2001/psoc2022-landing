25bbf6cdc9e4bfec26a9af0d7856b12f07122d6c
uav <uav0108@gmail.com>
2022-04-27 00:45:00 +0530

    modifed hero section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../styles/Navbar.module.scss'
 import Link from 'next/link'
-function navbar() {
+function Navbar() {
   return (
     <nav className={styles.nav}>
         <div className={styles.imgcontainer}>
@@ -11,10 +11,10 @@ function navbar() {
             <Link  href="/" passHref><p className={styles.link}>Home</p></Link>
             <Link  href="/timeline" passHref><p className={styles.link}>Timeline</p></Link>
             <Link  href="/about" passHref><p className={styles.link}>About</p></Link>
-            <Link  href="/logsign" passHref><p className={styles.link}>Login/Signup</p></Link>
+            {/* <Link  href="/logsign" passHref><p className={styles.link}>Login/Signup</p></Link> */}
         </div>
     </nav>
   )
 }
 
-export default navbar
+export default Navbar
