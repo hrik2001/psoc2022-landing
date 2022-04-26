9f63185786a7b437d7c548a760ee77af9b7af841
uav <uav0108@gmail.com>
2022-04-26 23:14:47 +0530

    Added Stats component, index.d.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Hero from '../components/Hero'
+import Stats from '../components/Stats'
+import Data from '../data/Stats'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Hero />
+      <div className={styles.stats}>
+        {Data.map((data, index)=>{
+          return (<Stats key={index} {...data} />)
+        })}
+      </div>
     </div>
   )
 }
