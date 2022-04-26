b850f7923b0d63ee1e2bd0a5c66a5581255bd0ee
Rispectech <ue208085.rishabhverma.it@gmail.com>
2022-04-26 05:57:12 +0530

    merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import WorkSection from "../components/WorkSection";
 import styles from "../styles/Home.module.scss";
-=======
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-import Hero from '../components/Hero'
->>>>>>> 91c1a891b4cd958cd731d134e513fc2e1808400a
+import Hero from "../components/Hero";
+import WorkSection from "../components/WorkSection";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-<<<<<<< HEAD
-      <WorkSection />
-=======
       <Hero />
->>>>>>> 91c1a891b4cd958cd731d134e513fc2e1808400a
+      <WorkSection />
     </div>
   );
 };
