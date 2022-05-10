79cb17dca64a24e8417f9f1ddadcec64d5a8fa12
Rispectech <ue208085.rishabhverma.it@gmail.com>
2022-05-11 00:41:51 +0530

    answer image

--- data/Projects.ts
+++ data/Projects.ts
@@ -1,7 +1,7 @@
 const projects: ProjectsProps[] = [
   {
     src: "https://github.com/hrik2001/answer",
-    img: "",
+    img: "/logo/answer.svg",
     name: "!answer",
     descr:
       "!answer is a discord bot that leverages state of the art artificial intelligence to serve your users in your discord guild. You can give the bot context paragraphs, from which the bot will reply to any questions of users.",
